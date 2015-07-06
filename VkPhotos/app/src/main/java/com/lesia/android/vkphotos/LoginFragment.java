31e3db0181144768aa80059a9bf554f4e15a7375
lesia <lesia.balovsyak@gmail.com>
2015-07-06 18:29:49 +0300

    Add saving authrization info in SharedPreferences

--- VkPhotos/app/src/main/java/com/lesia/android/vkphotos/LoginFragment.java
+++ VkPhotos/app/src/main/java/com/lesia/android/vkphotos/LoginFragment.java
@@ -27,22 +27,47 @@ public class LoginFragment extends Fragment {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 final String ACCESS_TOKEN = "access_token=";
                 final String EXPIRES_IN = "&expires_in=";
+                final String USER_ID = "&user_id=";
 
                 if(url.contains(ACCESS_TOKEN)) {
-                    int begin = url.indexOf(ACCESS_TOKEN) + ACCESS_TOKEN.length();
-                    int end = url.indexOf(EXPIRES_IN);
+                    int begin_access_token = url.indexOf(ACCESS_TOKEN) + ACCESS_TOKEN.length();
+                    int end_access_token = url.indexOf(EXPIRES_IN);
+                    int begin_expires_in = end_access_token + EXPIRES_IN.length();
+                    int end_expires_in = url.indexOf(USER_ID);
+                    int begin_user_id = end_expires_in + USER_ID.length();
+                    int end_user_id = url.length() - 1;
 
                     SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString(getString(R.string.access_token_key), url.substring(begin, end));
+                    editor.putString(
+                            getString(R.string.access_token_key),
+                            url.substring(begin_access_token, end_access_token)
+                    );
+                    editor.putString(
+                            getString(R.string.expires_in_key),
+                            url.substring(begin_expires_in, end_expires_in)
+                    );
+                    editor.putString(
+                            getString(R.string.user_id_key),
+                            url.substring(begin_user_id, end_user_id)
+                    );
                     editor.commit();
 
                     Log.v("ACCESS_TOKEN", getActivity().getPreferences(Context.MODE_PRIVATE)
-                            .getString(
-                                    getString(R.string.access_token_key),
-                                    getString(R.string.access_token_def_value))
+                                    .getString(
+                                            getString(R.string.access_token_key),
+                                            getString(R.string.access_token_def_value))
                     );
-
+                    Log.v("EXPIRES_IN", getActivity().getPreferences(Context.MODE_PRIVATE)
+                                    .getString(
+                                            getString(R.string.expires_in_key),
+                                            getString(R.string.expires_in_def_value))
+                    );
+                    Log.v("USER_ID", getActivity().getPreferences(Context.MODE_PRIVATE)
+                                    .getString(
+                                            getString(R.string.user_id_key),
+                                            getString(R.string.user_id_def_value))
+                    );
                     EventBus.getDefault().post(new AuthEvent());
                     return true;
                 }
--- VkPhotos/app/src/main/java/com/lesia/android/vkphotos/MainActivity.java
+++ VkPhotos/app/src/main/java/com/lesia/android/vkphotos/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lesia.android.vkphotos;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -28,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         if(savedInstanceState == null) {
             if(!getPreferences(Context.MODE_PRIVATE).getString(
                             getString(R.string.access_token_key),
